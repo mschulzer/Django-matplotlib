b60cd0916d2a49e99c1b3895aa53a0fe016f80e2
Morten Schultz <ms@brondby-gym.dk>
2024-10-17 18:44:13 +0200

    fixed plotting

--- femtech/urls.py
+++ femtech/urls.py
@@ -3,5 +3,5 @@ from .views import home_view, plot_view
 
 urlpatterns = [
     path('', home_view, name="home-view"),
-    path('plot/', plot_view, name='plot_view'),
+    path('plot/', plot_view, name='plot-view'),
 ]
